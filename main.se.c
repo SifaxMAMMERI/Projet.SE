efdbd4e1187d963708b85bea2ff74e3980a6c1a9
SifaxMAMMERI <155175443+SifaxMAMMERI@users.noreply.github.com>
2023-12-29 19:08:46 +0100

    Update main.se.c

--- main.se.c
+++ main.se.c
@@ -19,14 +19,6 @@ void cleanup()
     sem_destroy(&mutex);
 }
 
-void transposerMatrice(int matrice[MAX_SIZE][MAX_SIZE], int result[MAX_SIZE][MAX_SIZE]) {
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < m1; j++) {
-            result[j][i] = matrice[i][j];
-        }
-    }
-}
-
 void sauvegarderMatrice(int matrice[MAX_SIZE][MAX_SIZE], const char *nomFichier) {
     FILE *fichier = fopen(nomFichier, "w");
     if (fichier == NULL) {
@@ -67,14 +59,14 @@ void chargerMatrice(int matrice[MAX_SIZE][MAX_SIZE], const char *nomFichier) {
 void *producteur(void *arg) {
     int ligne = *((int *)arg);
 
-    // Calculer la ligne de la matrice résultante A
+    // Calculer la ligne de la matrice rÃ©sultante A
     for (int j = 0; j < m2; j++) {
         int resultat = 0;
         for (int k = 0; k < m1; k++) {
             resultat += B[ligne][k] * C[k][j];
         }
 
-        // Mettre le résultat dans le tampon
+        // Mettre le rÃ©sultat dans le tampon
         sem_wait(&empty);
         sem_wait(&mutex);
         T[ligne] = resultat;
@@ -93,7 +85,7 @@ void *consommateur(void *arg) {
     for (int i = 0; i < n1; i++) {
         sem_wait(&full);
         sem_wait(&mutex);
-        A[i][colonne] = T[i];  // Place l'élément T[y] dans la matrice résultante A t
+        A[i][colonne] = T[i];  // Place l'Ã©lÃ©ment T[y] dans la matrice rÃ©sultante A t
         sem_post(&mutex);
         sem_post(&empty);
     }
@@ -111,11 +103,11 @@ int main() {
     sem_init(&mutex, 0, 1);
 
     
-    printf("donnez les dimensions de la matrice B (n1 m1, limitées à %d) : ", MAX_SIZE);
+    printf("donnez les dimensions de la matrice B (n1 m1, limitÃ©es Ã  %d) : ", MAX_SIZE);
     scanf("%d %d", &n1, &m1);
 
-    // Demander à l'utilisateur de saisir les dimensions de la matrice C
-    printf("donnez les dimensions de la matrice C (n2 m2, limitées à %d) : ", MAX_SIZE);
+    // Demander Ã  l'utilisateur de saisir les dimensions de la matrice C
+    printf("donnez les dimensions de la matrice C (n2 m2, limitÃ©es Ã  %d) : ", MAX_SIZE);
     scanf("%d %d", &n2, &m2);
 
     if (m1 != n2) {
@@ -124,38 +116,38 @@ int main() {
     }
 
     if (n1 > MAX_SIZE || m1 > MAX_SIZE || n2 > MAX_SIZE || m2 > MAX_SIZE) {
-        fprintf(stderr, "NON,les dimensions des matrices dépassent la taille maximale autorisée");
+        fprintf(stderr, "NON,les dimensions des matrices dÃ©passent la taille maximale autorisÃ©e");
         return EXIT_FAILURE;
     }
 
     
-    printf("donnez les éléments de la matrice B (%d x %d) :\n", n1, m1);
+    printf("donnez les Ã©lÃ©ments de la matrice B (%d x %d) :\n", n1, m1);
     for (int i = 0; i < n1; i++)
         for (int j = 0; j < m1; j++)
             scanf("%d", &B[i][j]);
 
-    printf("donnze les éléments de la matrice C (%d x %d) : ", n2, m2);
+    printf("donnze les Ã©lÃ©ments de la matrice C (%d x %d) : ", n2, m2);
     for (int i = 0; i < n2; i++)
         for (int j = 0; j < m2; j++)
             scanf("%d", &C[i][j]);
 
-    // Initialiser la matrice résultante A avec des 0
+    // Initialiser la matrice rÃ©sultante A avec des 0
     for (int i = 0; i < n1; i++)
         for (int j = 0; j < m2; j++)
             A[i][j] = 0;
 
-    // Créer les threads producteurs
+    // CrÃ©er les threads producteurs
     pthread_t threadsProducteurs[n1];
     for (int i = 0; i < n1; i++) {
-        int *ligne = malloc(sizeof(int)); // Allouer de la mémoire pour chaque index de ligne
+        int *ligne = malloc(sizeof(int)); // Allouer de la mÃ©moire pour chaque index de ligne
         *ligne = i;
         pthread_create(&threadsProducteurs[i], NULL, producteur, (void *)ligne);
     }
 
-    // Créer les threads consommateurs
+    // CrÃ©er les threads consommateurs
     pthread_t threadsConsommateurs[m2];
     for (int j = 0; j < m2; j++) {
-        int *colonne = malloc(sizeof(int)); // Allouer de la mémoire pour chaque index de colonne
+        int *colonne = malloc(sizeof(int)); // Allouer de la mÃ©moire pour chaque index de colonne
         *colonne = j;
         pthread_create(&threadsConsommateurs[j], NULL, consommateur, (void *)colonne);
     }
@@ -167,7 +159,7 @@ int main() {
         pthread_join(threadsConsommateurs[j], NULL);
 
     
-    printf("Matrice résultante A :\n");
+    printf("Matrice rÃ©sultante A :\n");
     for (int i = 0; i < n1; i++) {
         for (int j = 0; j < m2; j++)
             printf("%d ", A[i][j]);
